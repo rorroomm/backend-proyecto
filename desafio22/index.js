cd446c5cf46b925ed7a12945c4328a5cb1fd511a
Rodrigo Martinez <75483866+rorroomm@users.noreply.github.com>
2021-10-07 17:59:06 -0300

    desafio 22

---
+++ desafio22/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const faker = require('faker');
+
+app.use(express.json());
+
+/* ----------------------- FAKER ----------------------- */
+
+const productos = [ ];
+
+    //DESAFIO 22 - VISTA-TEST C/ QUERY PARAMS
+    //productos/vista-test?cant=5
+app.get('/productos/vista-test', (req, res) => {
+    const { cant } = req.query;
+
+    if(!cant) {
+        for (let i = 0; i < 10; i++){
+            const producto = {
+                nombre: faker.commerce.productName(),
+                precio: faker.commerce.price(),
+                foto: faker.image.imageUrl()
+            }
+            productos.push(producto);
+        }
+
+        res.send(productos);
+    }
+
+    if(cant == 0){
+        const noProd = "No hay productos";
+        res.send(noProd);
+    }
+
+    for(let i = 0; i < cant; i++){
+        const producto = {
+            nombre: faker.commerce.productName(),
+            precio: faker.commerce.price(),
+            foto: faker.image.imageUrl()
+        }
+        productos.push(producto);  
+        console.log(producto);
+    }
+
+    res.send(productos);
+})
+
+/* ----------------------- FAKER ----------------------- */
+
+
+/* ----------------------- SERVER + DB CONNECTION ----------------------- */
+
+app.listen(8080, ()=>{
+    console.log('DB driving driving on port 8080');
+})
+
+
+
